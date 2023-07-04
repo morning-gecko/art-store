27e6fde72d2da0fe751d79820198acc9a35b2f27
git@github.com <squally-bill.0f@icloud.com>
2023-07-04 18:35:31 -0500

    new deployment framework

---
+++ .azure-pipelines/iac.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+resourceGroupName="Art-Store-RG"
+cosmosAccountName="Art-Store-Cosmos-Account"
+databaseName="ArtDatabase"
+containerName="ArtContainer"
+partitionKeyPath="/Id"
+
+echo "Installing cosmosdb-preview Azure CLI extension..."
+az extension add --name cosmosdb-preview
+
+echo "Creating resource group, '$resourceGroupName'..."
+az group create --name $resourceGroupName --location centralus
+
+echo "Creating Cosmos DB account, '$cosmosAccountName'..."
+az cosmosdb create --name $cosmosAccountName --resource-group $resourceGroupName --kind GlobalDocumentDB --default-consistency-level "Session"
+
+echo "Creating database, '$databaseName'..."
+az cosmosdb sql database create --account-name $cosmosAccountName --resource-group $resourceGroupName --name $databaseName
+
+echo "Creating container, '$containerName'..."
+az cosmosdb sql container create --account-name $cosmosAccountName --database-name $databaseName --name $containerName --resource-group $resourceGroupName --partition-key-path $partitionKeyPath --throughput 400
