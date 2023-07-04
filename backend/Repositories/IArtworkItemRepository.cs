48259bdc4c290b79f2abfb4753c411afdf7e05fe
git@github.com <squally-bill.0f@icloud.com>
2023-07-04 18:39:45 -0500

    new repository

---
+++ backend/Repositories/IArtworkItemRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using api.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace api.Repositories
+{
+    public interface IArtworkItemRepository
+    {
+        Task<IEnumerable<ArtworkItem>> GetAllArtworks();
+        Task<ArtworkItem?> GetArtworkItem(long id);
+        Task<ArtworkItem> UpsertArtwork(ArtworkItem artworkItem);
+        Task DeleteArtwork(long id);
+    }
+}
+
