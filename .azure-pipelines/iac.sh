cc1c689fd572188ccb5b6f64f465e6044a844910
git@github.com <squally-bill.0f@icloud.com>
2023-07-04 22:03:05 -0500

    idempotence and create db account on free tier!

--- .azure-pipelines/iac.sh
+++ .azure-pipelines/iac.sh
@@ -6,17 +6,48 @@ databaseName="ArtDatabase"
 containerName="ArtContainer"
 partitionKeyPath="/Id"
 
-echo "Installing cosmosdb-preview Azure CLI extension..."
 az extension add --name cosmosdb-preview
 
-echo "Creating resource group, '$resourceGroupName'..."
-az group create --name $resourceGroupName --location centralus
+az group create \
+    --name $resourceGroupName \
+    --location centralus
 
-echo "Creating Cosmos DB account, '$cosmosAccountName'..."
-az cosmosdb create --name $cosmosAccountName --resource-group $resourceGroupName --kind GlobalDocumentDB --default-consistency-level "Session"
+az cosmosdb create --name $cosmosAccountName \
+    --resource-group $resourceGroupName \
+    --kind GlobalDocumentDB \
+    --default-consistency-level "Session" \
+    --enable-free-tier true
 
-echo "Creating database, '$databaseName'..."
-az cosmosdb sql database create --account-name $cosmosAccountName --resource-group $resourceGroupName --name $databaseName
+if [[ -z $(az cosmosdb sql database list --resource-group $resourceGroupName --account-name $cosmosAccountName) ]];
+then
+    echo "Creating database, '$databaseName'..."
+    az cosmosdb sql database create \
+        --account-name $cosmosAccountName \
+        --resource-group $resourceGroupName \
+        --name $databaseName
+else
+    echo 'Database, '$databaseName', already exists.'
+    az cosmosdb sql database show \
+        --resource-group $resourceGroupName \
+        --account-name $cosmosAccountName \
+        --name $databaseName
+fi
 
-echo "Creating container, '$containerName'..."
-az cosmosdb sql container create --account-name $cosmosAccountName --database-name $databaseName --name $containerName --resource-group $resourceGroupName --partition-key-path $partitionKeyPath --throughput 400
+if [[ -z $(az cosmosdb sql container list --resource-group $resourceGroupName --account-name $cosmosAccountName --database-name $databaseName) ]];
+then
+    echo "Creating container, '$containerName'..."
+    az cosmosdb sql container create \
+        --account-name $cosmosAccountName \
+        --database-name $databaseName \
+        --name $containerName \
+        --resource-group $resourceGroupName \
+        --partition-key-path $partitionKeyPath \
+        --throughput 400
+else 
+    echo 'Container, '$containerName', already exists.'
+    az cosmosdb sql container show \
+        --resource-group $resourceGroupName \
+        --account-name $cosmosAccountName \
+        --database-name $databaseName \
+        --name $containerName
+fi
