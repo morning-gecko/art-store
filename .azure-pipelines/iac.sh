242a00f00f0936e35e3692024a63a06faf2e572c
git@github.com <squally-bill.0f@icloud.com>
2023-07-05 20:51:29 -0500

    add app service iac

--- .azure-pipelines/iac.sh
+++ .azure-pipelines/iac.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
 
-resourceGroupName="Art-Store-RG"
-cosmosAccountName="art-store-cosmos-account"
-databaseName="ArtDatabase"
-containerName="ArtContainer"
+location="centralus"
+
+resourceGroupName="rg-art-store"
+
+cosmosAccountName="csmo-acct-art"
+databaseName="csmo-db-art"
+containerName="csmo-cont-art"
 partitionKeyPath="/Id"
+
+appServicePlanName="app-plan-art"
+appServiceName="web-app-art"
 
 az extension add --name cosmosdb-preview
 
 az group create \
     --name $resourceGroupName \
-    --location centralus
+    --location $location
 
 az cosmosdb create --name $cosmosAccountName \
     --resource-group $resourceGroupName \
@@ -18,7 +24,10 @@ az cosmosdb create --name $cosmosAccountName \
     --default-consistency-level "Session" \
     --enable-free-tier true
 
-if [[ -z $(az cosmosdb sql database list --resource-group $resourceGroupName --account-name $cosmosAccountName) ]];
+if [[ -z $(az cosmosdb sql database show \
+        --resource-group $resourceGroupName \
+        --account-name $cosmosAccountName \
+        --name $databaseName 2> /dev/null) ]];
 then
     echo "Creating database, '$databaseName'..."
     az cosmosdb sql database create \
@@ -27,13 +36,13 @@ then
         --name $databaseName
 else
     echo 'Database, '$databaseName', already exists.'
-    az cosmosdb sql database show \
-        --resource-group $resourceGroupName \
-        --account-name $cosmosAccountName \
-        --name $databaseName
 fi
 
-if [[ -z $(az cosmosdb sql container list --resource-group $resourceGroupName --account-name $cosmosAccountName --database-name $databaseName) ]];
+if [[ -z $(az cosmosdb sql container show \
+        --resource-group $resourceGroupName \
+        --account-name $cosmosAccountName \
+        --database-name $databaseName \
+        --name $containerName 2> /dev/null) ]];
 then
     echo "Creating container, '$containerName'..."
     az cosmosdb sql container create \
@@ -45,9 +54,37 @@ then
         --throughput 400
 else 
     echo 'Container, '$containerName', already exists.'
-    az cosmosdb sql container show \
+fi
+
+if [[ -z $(az appservice plan show \
+        --name $appServicePlanName \
         --resource-group $resourceGroupName \
-        --account-name $cosmosAccountName \
-        --database-name $databaseName \
-        --name $containerName
+        --query "name" \
+        --output tsv 2> /dev/null) ]];
+then
+    echo "Creating App Service Plan, '$appServicePlanName'..."
+    az appservice plan create \
+        --name $appServicePlanName \
+        --resource-group $resourceGroupName \
+        --location $location \
+        --is-linux \
+        --sku F1
+else
+    echo "App Service Plan, '$appServicePlanName', already exists."
+fi
+
+if [[ -z $(az webapp show \
+        --name $appServiceName \
+        --resource-group $resourceGroupName \
+        --query "name" \
+        --output tsv 2> /dev/null) ]];
+then
+    echo "Creating App Service, '$appServiceName'..."
+    az webapp create \
+        --resource-group $resourceGroupName \
+        --plan $appServicePlanName \
+        --name $appServiceName \
+        --runtime "DOTNETCORE:7.0"
+else
+    echo "App Service, '$appServiceName', already exists."
 fi
