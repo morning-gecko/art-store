500993d384298948465fedc45b04310ceef76564
git@github.com <squally-bill.0f@icloud.com>
2023-07-18 21:51:28 -0500

    navbar! and some products

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Product } from './models/product';
+import { Pagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Jim\'s Art Store';
+  products: Product[] = [];
+  constructor(private http: HttpClient) {}
+  
+  ngOnInit(): void {
+    this.http.get<Pagination<Product[]>>('https://localhost:5001/api/products?pageSize=50').subscribe({
+      next: response => this.products = response.data,
+      error: err => console.log(err), // what to do if error
+      complete: () => {
+        console.log('Request completed');
+        console.log('Extra statement');
+
+      }
+    });
+  }
 }
