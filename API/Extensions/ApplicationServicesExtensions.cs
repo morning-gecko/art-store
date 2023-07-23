dcb7395771a6fc5c8ce04744df51d1f0c955ac47
git@github.com <squally-bill.0f@icloud.com>
2023-07-22 22:32:52 -0500

    add more logging

--- API/Extensions/ApplicationServicesExtensions.cs
+++ API/Extensions/ApplicationServicesExtensions.cs
@@ -18,6 +18,7 @@ namespace API.Extensions
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             services.AddEndpointsApiExplorer();
             services.AddSwaggerGen();
+            Console.WriteLine("Setting up db context");
             services.AddDbContext<StoreContext>(opt =>
             {
                 Console.WriteLine(config.GetConnectionString("DefaultConnection"));
