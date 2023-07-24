79228f2d509271442d7f566d4d51cb5f23a6bc13
Drew <squally-bill.0f@icloud.com>
2023-07-23 22:14:41 -0500

    Deploy frontend (#3)
    * refactor dev urls for remote deployment
    
    * temp placeholders from public domain
    
    * temp placeholders from public domain
    
    * need renames to differentiate website assets
    
    * remove redundant file copy
    
    * resolve odd warnings in ng build
    
    * app must launch from same dir as API.dll
    
    * more pages, more relevant test data

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ShopComponent } from './shop/shop.component';
 import { ProductDetailsComponent } from './shop/product-details/product-details.component';
+import { ContactComponent } from './contact/contact.component';
+import { BlogComponent } from './blog/blog.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'shop', component: ShopComponent },
   { path: 'shop/:id', component: ProductDetailsComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'blog', component: BlogComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
 
