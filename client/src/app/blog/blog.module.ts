b8e13df5fb23f6149896c740150e390f8b5dfdf7
git@github.com <squally-bill.0f@icloud.com>
2023-07-23 22:13:04 -0500

    more pages, more relevant test data

---
+++ client/src/app/blog/blog.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BlogComponent } from './blog.component';
+import { SharedModule } from '../shared/shared.module';
+
+
+
+@NgModule({
+  declarations: [
+    BlogComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedModule
+  ]
+})
+export class BlogModule { }
