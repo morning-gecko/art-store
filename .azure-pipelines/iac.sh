97d1f6f66e1d8d157c056fc0ad452e1d5aa0be89
git@github.com <squally-bill.0f@icloud.com>
2023-08-14 21:15:00 -0500

    add database deploy and idempotence

--- .azure-pipelines/iac.sh
+++ .azure-pipelines/iac.sh
@@ -88,25 +88,56 @@ az postgres db create \
     --server-name $pgServerName \
     --name $artDbName
 
-az postgres flexible-server create \
-    --name $idServerName \
-    --resource-group $resourceGroupName \
-    --location $location \
-    --admin-user $idDbUser \
-    --admin-password $idDbPassword \
-    --sku-name Standard_B1ms \
-    --tier Burstable \
-    --version 13 \
-    --yes
+if [[ -z $(az postgres flexible-server show \
+        --name $idServerName \
+        --resource-group $resourceGroupName \
+        --query "name" \
+        --output tsv 2> /dev/null) ]];
+then
+    echo "Creating Flexible Server, '$idServerName'..."
+    az postgres flexible-server create \
+        --name $idServerName \
+        --resource-group $resourceGroupName \
+        --location $location \
+        --admin-user $idDbUser \
+        --admin-password $idDbPassword \
+        --sku-name Standard_B1ms \
+        --tier Burstable \
+        --version 13 \
+        --yes
+else
+    echo "Flexible Server, '$idServerName', already exists."
+fi
 
-az postgres server firewall-rule create \
-    --resource-group $resourceGroupName \
-    --server-name $pgServerName \
-    --name AllowAllAzureIPs \
-    --start-ip-address '0.0.0.0' \
-    --end-ip-address '0.0.0.0'
+if [[ -z $(az postgres flexible-server firewall-rule show \
+        --resource-group $resourceGroupName \
+        --server-name $idServerName \
+        --name AllowAzureServices \
+        --query "name" \
+        --output tsv 2> /dev/null) ]];
+then
+    echo "Creating firewall rule for Flexible Server, '$idServerName'..."
+    az postgres flexible-server firewall-rule create \
+        --resource-group $resourceGroupName \
+        --name $idServerName \
+        --rule-name AllowAzureServices \
+        --start-ip-address '0.0.0.0'
+else
+    echo "Firewall rule, 'AllowAzureServices', for Flexible Server, '$idServerName', already exists."
+fi
 
-az postgres db create \
-    --resource-group $resourceGroupName \
-    --server-name $pgServerName \
-    --name $idDbName
+if [[ -z $(az postgres flexible-server db show \
+        --resource-group $resourceGroupName \
+        --server-name $idServerName \
+        --name $idDbName \
+        --query "name" \
+        --output tsv 2> /dev/null) ]];
+then
+    echo "Creating database, '$idDbName', for Flexible Server, '$idServerName'..."
+    az postgres flexible-server db create \
+        --resource-group $resourceGroupName \
+        --server-name $idServerName \
+        --database-name $idDbName
+else
+    echo "Database, '$idDbName', already exists."
+fi
